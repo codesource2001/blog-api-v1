14c09e5596ef18c4777a8fd4ee27d94936dd5122
Code Source <codesource2001@gmail.com>
2026-02-04 22:24:32 +0530

    global error handling

--- src/server.js
+++ src/server.js
@@ -4,4 +4,12 @@ const routes = require("./routes");
 
 app.use("/", routes.testRouter);
 
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal server error!";
+  res.status(err.statusCode).json({ success: false, error: err.message });
+  next();
+});
+
 module.exports = app;
